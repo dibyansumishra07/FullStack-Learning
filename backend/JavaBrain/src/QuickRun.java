6add17623687825e833d1fa41cd70f98b5def85e
dibyansumishra07 <dibyansu.mishra07@gmail.com>
2025-03-02 21:17:50 +0530

    starting with java code base

---
+++ backend/JavaBrain/src/QuickRun.java
@@ -0,0 +1,18 @@
+public class QuickRun {
+    private static String test= "test";
+    public static void main(String[] args) {
+        String s = "AACCDDDBBBE";
+        System.out.println("Hello");
+        char currentChar = s.charAt(0);
+        char maxChar = s.charAt(0);
+        int currentCount = 0;
+        int maxCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            currentChar = s.charAt(i);
+            if(currentChar == maxChar) {
+
+            }
+
+        }
+    }
+}
